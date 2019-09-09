02b913a6e02881a26c9b0efd6dd6211b8c0d39c3
David Verbenyi <dverbenyi@gmail.com>
2019-09-09 18:24:15 +0100

    Indentation fix either spaces or tabs

--- controllers/landing.js
+++ controllers/landing.js
@@ -1,7 +1,7 @@
 
 
 exports.get_landing = function(req, res, next) {
-  res.render('index', { title: 'David\'s Page' });
+  res.render('landing', { title: 'David\'s Page' });
 }
 
 exports.submit_email = function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,6 @@ let landing = require('../controllers/landing')
 
 /* GET home page. */
 router.get('/', landing.get_landing);
-// router.post('/', landing.submit_email);
+router.post('/', landing.submit_email);
 
 module.exports = router;
