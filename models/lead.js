24a026e97ea0e05ebc4790628bd1c91a65378ff1
David Verbenyi <dverbenyi@gmail.com>
2019-09-11 10:58:37 +0100

    add model to sequelize

---
+++ models/lead.js
@@ -0,0 +1,18 @@
+'use strict';
+module.exports = (Sequelize, DataTypes) => {
+	var Lead = sequelize.define('Lead', {
+		id: {
+			type: DataTypes.UUID,
+			defaultValue: DataTypes.UUIDV4,
+
+			allowNull: false,
+			primaryKey: true
+		},
+		email: {
+			type: DataTypes.STRING,
+			allowNull: false,
+		},
+	});
+
+	return Lead;
+};
