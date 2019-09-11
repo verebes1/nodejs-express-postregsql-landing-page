14ce52e6c630cbe4995b4e3eff12875faae83673
David Verbenyi <dverbenyi@gmail.com>
2019-09-11 12:26:42 +0100

    Add update method

--- controllers/landing.js
+++ controllers/landing.js
@@ -31,3 +31,26 @@ exports.show_lead = function(req, res, next) {
 		res.render('lead', {lead: lead});
 	});
 }
+
+exports.show_edit_lead = function(req, res, next) {
+	return models.Lead.findOne({
+		where: {
+			id: req.params.lead_id
+		}
+	}).then(lead => {
+		res.render('lead/edit_lead', {lead: lead});
+	});
+}
+
+exports.edit_lead = function(req, res, next) {
+	console.log("UPDATE CALLED")
+	return models.Lead.update({
+		email: req.body.lead_email
+	}, {
+		where: {
+			id: req.params.lead_id
+		}
+	}).then(result => {
+		res.redirect('/lead/' + req.params.lead_id);
+	})
+}
